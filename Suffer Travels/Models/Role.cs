b779948aec6814b41902eb3d1a0523fd083b370e
shreyanshu28 <shreyanshuvyas@gmail.com>
2022-01-30 22:19:22 +0530

    User model

---
+++ Suffer Travels/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Suffer_Travels.Models
+{
+    public class Role
+    {
+        [Key]
+        public UInt32 Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
---
+++ Suffer Travels/Models/User.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Suffer_Travels.Models
+{
+    public class User
+    {
+        [Key]
+        public UInt32 UId { get; set; }
+        [Required]
+        public string Fname { get; set; }
+        public string Mname { get; set; }
+        [Required]
+        public string Lname { get; set; }
+
+        public char Gender { get; set; } = 'M';
+        public DateOnly DateOfBirth { get; set; } = DateOnly.MinValue;
+        [Required]
+        public Int64 ContactNo { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string ProfilePhoto { get; set; }
+        public Boolean IsActive { get; set; } = true;
+        [ForeignKey("Role")]
+        public UInt32 RoleId { get; set; }  
+
+    }
+}
