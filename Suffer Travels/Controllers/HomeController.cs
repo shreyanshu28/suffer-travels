311f73f4d2cfbd4a5104da5f153fd84c5a894441
shreyanshu28 <shreyanshuvyas@gmail.com>
2022-02-01 09:57:08 +0530

    Login moved to user controller

--- Suffer Travels/Controllers/HomeController.cs
+++ Suffer Travels/Controllers/HomeController.cs
@@ -23,10 +23,6 @@ namespace Suffer_Travels.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
