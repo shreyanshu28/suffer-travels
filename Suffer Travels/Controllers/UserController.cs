1186c3eb73174f983e58c8556cb7642ccd040bfc
kushal <kushalgaywala@gmail.com>
2022-02-01 17:30:08 +0530

    Otp function register not working

--- Suffer Travels/Controllers/UserController.cs
+++ Suffer Travels/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Suffer_Travels.Data;
 using Suffer_Travels.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace Suffer_Travels.Controllers
 {
@@ -60,12 +62,42 @@ namespace Suffer_Travels.Controllers
         public IActionResult VerifyUser()
         {
             ViewData["fname"] = TempData["fname"];
+            CreateCopyMessage("smtp.gmail.com");
             //Console.WriteLine(user.Fname);
             //ViewData["user"] = user;
 
             return View();
         }
 
+        public static void CreateCopyMessage(string server)
+        {
+            MailAddress from = new MailAddress("19bmiit052@gmail.com", "Kushal Gaiwala");
+            MailAddress to = new MailAddress("19bmiit112@gmail.com", "Shreyanshu Vyas");
+            MailMessage message = new MailMessage(from, to);
 
+            message.Subject = "Using the SmtpClient class.";
+            message.Body = @"Using this feature, you can send an email message from an application very easily.";
+            
+            // Add a carbon copy recipient.
+            //MailAddress copy = new MailAddress("Notification_List@contoso.com");
+            //message.CC.Add(copy);
+
+            SmtpClient client = new SmtpClient(server);
+            
+            // Include credentials if the server requires them.
+            client.Credentials = CredentialCache.DefaultNetworkCredentials;
+            Console.WriteLine("Sending an email message to {0} by using the SMTP host {1}.",
+                 to.Address, client.Host);
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in CreateCopyMessage(): {0}",
+                    ex.ToString());
+            }
+        }
     }
 }
