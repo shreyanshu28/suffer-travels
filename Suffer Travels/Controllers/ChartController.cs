3bcf54f0d4e0f88b7b8be88cf724af2c8dd177d9
kushal <kushalgaywala@gmail.com>
2022-02-05 17:02:12 +0530

    Chart JS Testing new Controller

---
+++ Suffer Travels/Controllers/ChartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Suffer_Travels.Data;
+using Suffer_Travels.Models;
+
+namespace Suffer_Travels.Controllers
+{
+    public class ChartController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ChartController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<User> user = db.tblUser;
+            ViewData["User"] = user;
+            return View(user);
+        }
+    }
+}
