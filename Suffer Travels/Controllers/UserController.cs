4e88a45549c76197af9e1375a9d8e156a379de7b
kushal <kushalgaywala@gmail.com>
2022-02-08 00:13:57 +0530

    Now can send otp

--- Suffer Travels/Controllers/UserController.cs
+++ Suffer Travels/Controllers/UserController.cs
@@ -54,44 +54,100 @@ namespace Suffer_Travels.Controllers
         {
             //db.Add(user);
             //db.SaveChanges();
-            TempData.Add("fname", user.Fname);
+            TempData.Add("tmpFname", user.Fname);
+            TempData.Add("tmpMname", user.Mname);
+            TempData.Add("tmpLname", user.Lname);
+            TempData.Add("tmpDOB", user.DateOfBirth);
+            TempData.Add("tmpContactNo", user.ContactNo.ToString());
+            TempData.Add("tmpEmail", user.Email);
 
             return RedirectToAction("VerifyUser");
         }
 
+        public string tempDataToString(Object tempData)
+        {
+            if(tempData != null)
+                return tempData.ToString();
+            return "";
+        }
+
         public IActionResult VerifyUser()
         {
-            ViewData["fname"] = TempData["fname"];
-            CreateCopyMessage("smtp.gmail.com");
-            //Console.WriteLine(user.Fname);
-            //ViewData["user"] = user;
+            int otp = 0;
 
+            string fname = "", mname = "", lname = "", gender = "", dob = "", contactNo = "", email = "";
+            
+            fname = tempDataToString(TempData["tmpFname"]);
+            mname = tempDataToString(TempData["tmpMname"]);
+            lname = tempDataToString(TempData["tmpLname"]);
+            gender = tempDataToString(TempData["tmpGender"]);
+            dob = tempDataToString(TempData["tmpDOB"]);
+            contactNo = tempDataToString(TempData["tmpContactNo"]);
+            email = tempDataToString(TempData["tmpEmail"]);
+
+            //if (TempData.ContainsKey("tmpFname"))
+            //    fname = TempData["tmpFname"].ToString();
+
+            //if (TempData.ContainsKey("tmpMname"))
+            //    mname = TempData["tmpMname"].ToString();
+
+            //if (TempData.ContainsKey("tmpLname"))
+            //    lname = TempData["tmpLname"].ToString();
+
+            //if (TempData.ContainsKey("tmpGender"))
+            //    gender = TempData["tmpGender"].ToString();
+
+            //if (TempData.ContainsKey("tmpDOB"))
+            //    dob = TempData["tmpDOB"].ToString(); 
+
+            //if (TempData.ContainsKey("tmpContactNo"))
+            //    contactNo = TempData["tmpContactNo"].ToString(); 
+
+            //if (TempData.ContainsKey("tmpEmail"))
+            //    email = TempData["tmpEmail"].ToString();
+
+            otp = sendOtp(email);
+
+            TempData.Clear();
             return View();
         }
 
-        public static void CreateCopyMessage(string server)
+        public static int sendOtp( string toEmail)
         {
+            int otp = 0;
             string email = "kushal8217@gmail.com", pass = "kushalkushal8217";
+            string server = "smtp.gmail.com";
+
             MailAddress from = new MailAddress("kushal8217@gmail.com", "Kushal Gaiwala");
-            MailAddress to = new MailAddress("19bmiit112@gmail.com", "Shreyanshu Vyas");
+            MailAddress to = new MailAddress(toEmail, "Shreyanshu Vyas");
             MailMessage message = new MailMessage(from, to);
 
-            message.Subject = "Using the SmtpClient class.";
-            message.Body = @"Using this feature, you can send an email message from an application very easily.";
-            
+            Random rand = new Random();
+            otp = rand.Next(111111, 999999);
+
+            string strOtp = @"<span style='font-weight: bold; font-size: 25px;'>" 
+                                + otp.ToString() 
+                            + "</span>";
+
+            message.Subject = "Suffer Travels";
+            message.IsBodyHtml = true;
+            message.Body = @"<h3>Please do not share this otp to anyone</h3>
+                            <p>The OTP for verification is</p>
+                            <b>" + strOtp + "</b>";
+
             // Add a carbon copy recipient.
             //MailAddress copy = new MailAddress("Notification_List@contoso.com");
             //message.CC.Add(copy);
-
+            
             SmtpClient client = new SmtpClient(server);
 
             // Include credentials if the server requires them.
             client.Port = 587;
-            client.Credentials = new System.Net.NetworkCredential(from.Address, pass);
+            client.Credentials = new NetworkCredential(from.Address, pass);
             client.EnableSsl = true;
 
-            Console.WriteLine("Sending an email message to {0} by using the SMTP host {1}.",
-                 to.Address, client.Host);
+            Console.WriteLine("Sending an email message to {0} by using the SMTP host {1}.", 
+                to.Address, client.Host);
 
             try
             {
@@ -101,7 +157,9 @@ namespace Suffer_Travels.Controllers
             {
                 Console.WriteLine("Exception caught in CreateCopyMessage(): {0}",
                     ex.ToString());
+                return 0;
             }
+            return otp;
         }
     }
 }
