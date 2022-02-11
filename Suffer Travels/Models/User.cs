be9b0cad4a1afd4077590a892472e73ee2dbf5cc
kushal <kushalgaywala@gmail.com>
2022-02-11 11:03:07 +0530

    Validation for user model

--- Suffer Travels/Models/User.cs
+++ Suffer Travels/Models/User.cs
@@ -8,26 +8,33 @@ namespace Suffer_Travels.Models
     {
         [Key]
         public UInt32 UId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "First name is required!")]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "First name should only contain alphabets!")]
         public string Fname { get; set; }
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Middle name should only contain alphabets!")]
         public string Mname { get; set; } = "";
-        [Required]
+        [Required(ErrorMessage = "Last name is required!")]
+        [RegularExpression("[a-zA-Z]+", ErrorMessage = "Last name should only contain alphabets!")]
         public string Lname { get; set; }
+        [Required(ErrorMessage = "Gender is required!")]
         public char Gender { get; set; } = 'M';
         [Required]
         public DateTime DateOfBirth { get; set; }
-        [Required]
-        [Phone(ErrorMessage = "Enter a valid phone number")]
+        //[Required(ErrorMessage = "Contact number is required!")]
+        //[RegularExpression("^[7-9][0-9]+", ErrorMessage = "Please enter valid contact number")]
+        //[DataType(DataType.PhoneNumber, ErrorMessage = "Please enter valid contact number")]
+        //[MaxLength(10, ErrorMessage = "Please enter valid contact number")]
+        //[MinLength(10, ErrorMessage = "Please enter valid contact number")]
         public long ContactNo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Email address is required!")]
         [EmailAddress(ErrorMessage = "Enter a valid email address")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Username is required!")]
         [StringLength(50, ErrorMessage = "Username should have 3 to 50 characters only", MinimumLength = 3)]
         public string Username { get; set; } = "123";
-        [Required]
+        [Required(ErrorMessage = "Password is required!")]
         [StringLength(20, ErrorMessage = "Password minimum 8 character", MinimumLength = 8)]
-        public string Password { get; set; } = "123";
+        public string Password { get; set; }
         public string ProfilePhoto { get; set; } = "";
         public Boolean IsActive { get; set; } = true;
         [ForeignKey("Role")]
