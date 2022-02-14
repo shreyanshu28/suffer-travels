9aea8f0c8a342cd346e75b01bf5065d8dfba5fd7
kushal <kushalgaywala@gmail.com>
2022-02-14 22:15:15 +0530

    otp send jquery

--- Suffer Travels/Controllers/UserController.cs
+++ Suffer Travels/Controllers/UserController.cs
@@ -9,7 +9,7 @@ namespace Suffer_Travels.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext db;
-        private static int otp;
+        private int otp;
         public UserController(ApplicationDbContext _db)
         {
             db = _db;
@@ -101,14 +101,21 @@ namespace Suffer_Travels.Controllers
         }*/
 
         [HttpPost]
+        public ActionResult SendOtp(Register register)
+        {
+            sendOtp(register.Email, "kushal");
+
+            return Json( new { sendOtp = otp } );
+        }
+
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult AddPassword(Register register, int? id)
         {
             IEnumerable<User> _user = db.tblUser;
             if (id != 1)
             {
-                return RedirectToAction("HomePage");
-                /*if (register.Email != null)
+                if (register.Email != null)
                 {
                     if (!_user.Any(u => u.Email == register.Email))
                     {
@@ -120,7 +127,7 @@ namespace Suffer_Travels.Controllers
                         otp = sendOtp(register.Email, "Forgot Password");
                         return RedirectToAction("VerifyUser");
                     }
-                }*/
+                }
             }
 
 
@@ -190,14 +197,14 @@ namespace Suffer_Travels.Controllers
             return "";
         }
 
-        public static int sendOtp(string toEmail, string username)
+        public int sendOtp(string toEmail, string username)
         {
             
             string email = "kushal8217@gmail.com", pass = "kushalkushal8217";
             string server = "smtp.gmail.com";
 
             MailAddress from = new MailAddress("kushal8217@gmail.com", "Kushal Gaiwala");
-            MailAddress to = new MailAddress(toEmail, username);
+            MailAddress to = new MailAddress(toEmail, "shreyanshu vyas");
             MailMessage message = new MailMessage(from, to);
 
             Random rand = new Random();
---
+++ Suffer Travels/wwwroot/js/user/AddPassword.js
@@ -0,0 +1,28 @@
+﻿const getOtp = document.getElementById("GetOtp");
+
+$(document).ready(() => {
+    $('#GetOtp').click((event) => {
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("SendOtp", "User")',
+            data: {
+                email: $('#email').val(),
+            },
+            error: function (result) {
+                $('#OtpMsg').val(() => "Otp not sent");
+                //alert("There is a Problem, Try Again!");
+            },
+            success: function (result) {
+                console.log(result);
+                if (result.status == true) {
+                    $('#OtpMsg').val(() => "Otp sent");
+                    getOtp.classList.remove("text-danger");
+                    getOtp.classList.add("text-success");
+                }
+                else {
+                    alert(result.message);
+                }
+            }
+        });
+    });
+});
