34c098fee579a83c19539ad4c09b688c47bfaec5
kushal <kushalgaywala@gmail.com>
2022-02-19 18:02:13 +0530

    Updated TourViewModel - Added individual objects of models - Added order controller with details still working

--- Suffer Travels/Controllers/OrderController.cs
+++ Suffer Travels/Controllers/OrderController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Suffer_Travels.Data;
+using Suffer_Travels.Models;
+using Suffer_Travels.ViewModel;
 
 namespace Suffer_Travels.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly ApplicationDbContext db;
+        public OrderController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,12 +23,26 @@ namespace Suffer_Travels.Controllers
             return View();
         }
 
-        public IActionResult AddOrder()
+        public IActionResult AddOrder(int? id)
         {
+            if (string.IsNullOrWhiteSpace(HttpContext.Session.GetString("Email")))
+            {
 
-            ViewData["IsLoggedin"] = string.IsNullOrWhiteSpace(HttpContext.Session.GetString("Email"));
+                ViewData["IsLoggedIn"] = "Not";
+            }
+            else
+            {
+                ViewData["IsLoggedin"] = "LoggedIn";
+            }
 
-            return View();
+            TourViewModel tourViewModel = new TourViewModel();
+            tourViewModel.tourDetail = db.tblTour.FirstOrDefault(tour => tour.TId == id);
+            tourViewModel.tourType = db.tblTourType.FirstOrDefault(tour => tour.);
+            tourViewModel.tourDate = db.tblTourDates.FirstOrDefault(tour => tour.);
+            tourViewModel.tourPhoto = db.tblTourPhotos.FirstOrDefault(tour => tour.);
+            tourViewModel.photo = db.tblPhotos.FirstOrDefault(tour => tour.);
+
+            return View(tourViewModel);
         }
     }
 }
--- Suffer Travels/ViewModel/TourViewModel.cs
+++ Suffer Travels/ViewModel/TourViewModel.cs
@@ -5,11 +5,17 @@ namespace Suffer_Travels.ViewModel
 {
     public class TourViewModel
     {
-        public Tour tours { get; set; }
+        public Tour tour { get; set; }
+        public TourType tourType { get; set; }
+        public TourDates tourDate { get; set; }
+        public TourPhotos tourPhoto { get; set; }
+        public Photo photo { get; set; }
 
         public IEnumerable<Tour> tourDetails { get; set; }
 
         public IEnumerable<TourType> tourTypes { get; set; }
+
+        public IEnumerable<TourDates> tourDates { get; set; }
 
         public IEnumerable<TourPhotos> tourPhotos { get; set; }
 
